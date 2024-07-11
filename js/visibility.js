8a48f1a3edf603fafb32d02a95cf21e2d4cc928a
minseokim <claudia503@naver.com>
2024-07-11 09:34:13 +0900

    mission 3 Initial commit

--- js/visibility.js
+++ js/visibility.js
@@ -2,5 +2,5 @@ function togglePasswordVisibility(fieldId, toggleElement) {
     const field = document.getElementById(fieldId);
     const isPasswordVisible = field.type === 'text';
     field.type = isPasswordVisible ? 'password' : 'text';
-    toggleElement.querySelector('img').src = 'Vector.svg';
+    toggleElement.querySelector('img').src = '/image/visibility.svg';
 }
