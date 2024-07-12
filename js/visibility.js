9aa0f1073f83b219d0f88559fd7da2b96c97d2cc
minseokim <claudia503@naver.com>
2024-07-13 00:01:16 +0900

    refactor: mission2 눈모양 표시 수정

--- js/visibility.js
+++ js/visibility.js
@@ -2,5 +2,5 @@ function togglePasswordVisibility(fieldId, toggleElement) {
     const field = document.getElementById(fieldId);
     const isPasswordVisible = field.type === 'text';
     field.type = isPasswordVisible ? 'password' : 'text';
-    toggleElement.querySelector('img').src = '/image/visibility.svg';
+    toggleElement.querySelector('img').src = isPasswordVisible ? '/image/invisible.svg' : '/image/visible.svg';
 }
