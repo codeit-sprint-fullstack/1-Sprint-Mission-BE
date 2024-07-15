1b39b51cb1b5bfc17c1ee63807fe2e670dfffc7a
jongmin <charlieko123@github.com>
2024-07-15 21:45:05 +0900

    add getArticle method

--- ArticleService.js
+++ ArticleService.js
@@ -1,1 +1,29 @@
-const res = await fetch('https://sprint-mission-api.vercel.app/articles');
+const basicUrl = 'https://sprint-mission-api.vercel.app/articles';
+
+export async function getArticleList(){
+  const params = {
+    page: 1,
+    pageSize: 30,
+    keyword: '추천'
+  };
+  const url = new URL(basicUrl);
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+
+  const res = await fetch(url);
+  const data = await res.json();
+  
+  console.log(data);
+}
+
+export async function getArticle(id){
+  const url = `${basicUrl}/${id}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data);
+}
+
+
---
+++ main.js
@@ -0,0 +1,4 @@
+import { getArticleList, getArticle } from './ArticleService.js';
+
+// getArticleList();
+getArticle(5);
