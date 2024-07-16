4aadbf4b7e31cc939ab1b251bac0f4eee4aaba20
sanho <dieuaimer@gmail.com>
2024-07-16 16:47:16 +0900

    feat: Add getProductList function and getProduct function

--- ProductService.js
+++ ProductService.js
@@ -1,5 +1,23 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: ''
-})
+  baseURL: 'https://sprint-mission-api.vercel.app',
+  timeout: 3000,
+});
+
+export async function getProductList(options = {}) {
+  const {page=1, pageSize=100, keyword} = options;
+  const params = {
+    page: page,
+    pageSize: pageSize,
+    keyword: keyword || undefined,
+  }
+
+  const res = await instance.get('/products', { params });
+  return res.data;
+}
+
+export async function getProduct(id) {
+  const res = await instance.get(`/products/${id}`);
+  return res.data;
+}
