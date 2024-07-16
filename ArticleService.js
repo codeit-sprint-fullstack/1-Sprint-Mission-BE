2396c0b55027ad977974f3cb614d0317a58a9b84
sanho <dieuaimer@gmail.com>
2024-07-16 14:19:31 +0900

    feat: Add createArticle function

--- ArticleService.js
+++ ArticleService.js
@@ -52,3 +52,47 @@ export function getArticle(id) {
     return null;
   })
 }
+
+export function createArticle(data) {
+  const url = new URL('https://sprint-mission-api.vercel.app/articles');
+
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`Network response is not 2XX: ${res.status}`)
+    }
+    return res.json();
+  })
+  .catch((e) => {
+    console.error('Error: ', e.message);
+    return null;
+  })
+}
+
+export function patchArticle(data) {
+  const url = new URL(`https://sprint-mission-api.vercel.app/articles/${id}`);
+
+  return fetch(url, {
+    method: 'PATCH',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error(`Network response is not 2XX: ${res.status}`);
+    }
+    return res.json();
+  })
+  .catch((e) => {
+    console.error('Error: ', e.message);
+    return null;
+  })
+}
--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-import { getArticleList, getArticle } from './ArticleService.js'
+import { getArticleList, getArticle, createArticle } from './ArticleService.js'
 
-
-getArticleList({page:1, pageSize:5})
+getArticleList({page: 1, pageSize: 2})
   .then(result => {
     if (result) {
-      console.log('ArticleList: ', JSON.stringify(result, null, 2));
+      console.log('ArticleList: ', result, '\n');
     } else {
-      console.log('No results returned');
+      console.log('No results returned\n');
     }
   })
 
@@ -14,8 +13,27 @@ getArticleList({page:1, pageSize:5})
 getArticle(10)
   .then(result => {
     if (result) {
-      console.log('Article: ', JSON.stringify(result, null, 2));
+      console.log('Article: ', result, '\n');
     } else {
-      console.log('No results returned')
+      console.log('No results returned\n')
     }
   })
+
+
+const articles = [
+  {title: '감자', content: '[판다마켓] 강원도 감자(햇), 1kg', image: 'https://thumbnail7.coupangcdn.com/thumbnails/remote/492x492ex/image/retail/images/6597174365713211-338aa9b8-a411-4d36-8127-24be7159bad0.jpg'},
+  {title: '피데기', content: '[판다마켓] 포항 구룡포 국내산 피데기 반건조오징어 10마리(700g 내외)', image: 'https://imagecdn.skstoa.com/goods/422/26466422_g.jpg'},
+  {title: '랍스타', content: '[판다마켓] 살아서 도착하는 캐나다산 활랍스타 1마리(1kg 내외)', image: 'https://sitem.ssgcdn.com/99/24/95/item/1000081952499_i1_750.jpg'},
+]
+
+Promise.all(articles.map(article => createArticle(article)))
+  .then(results => {
+    results.forEach((result, idx) => {
+      if (result) {
+        console.log(`Article ${idx + 1} saved successfully:`, result);
+      } else {
+        console.log(`Failed to save Article ${idx + 1}`);
+      }
+    });
+  console.log('\n');  
+  });
