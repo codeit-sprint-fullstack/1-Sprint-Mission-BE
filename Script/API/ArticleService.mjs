cebaa71d2def97120c653c6f4c6105cfd02918e9
Kim HyunU <recom54@gmail.com>
2024-07-16 11:23:55 +0900

    funcAddInMain

--- Script/API/ArticleService.mjs
+++ Script/API/ArticleService.mjs
@@ -49,9 +49,9 @@ export async function getArticle() {
 }
 
 /** 게시글 작성 함수
- * @param {string} title 등록 제품의 이름을 넣자
- * @param {string} content 등록 제품의 설명을 넣자
- * @param {string} image 등록 제품의 가격을 넣자
+ * @param {string} title 글의 제목을 넣자
+ * @param {string} content 글의 내용을 넣자
+ * @param {Array} image 글의 이미지를 배열로 넣자
  */
 export function createArticle(
   title,
@@ -73,10 +73,10 @@ export function createArticle(
 }
 
 /** 게시글 수정 함수
- * @param {number} ID 글의 ID를 넣자
- * @param {string} title 글의 제목을 넣자
- * @param {string} content 글의 내용을 넣자
- * @param {number} image 글의 이미지를 넣자
+ * @param {number} ID 수정할 글의 ID를 넣자
+ * @param {string} title 수정할 글의 제목을 넣자
+ * @param {string} content 수정할 글의 내용을 넣자
+ * @param {Array} image 수정할 글의 이미지를 배열로 넣자
  */
 export function patchArticle(ID, title, content, image) {
   const apiUrl = `https://sprint-mission-api.vercel.app/articles/${ID || null}`;
@@ -102,10 +102,10 @@ export function deleteArticle(ID) {
   axios
     .delete(apiUrl)
     .then(() => {
-      console.log(`${ID} 번 제품을 삭제했습니다`);
+      console.log(`${ID} 번 게시글을 삭제했습니다`);
     })
     .catch((error) => {
-      console.error(`${ID} 번 제품 삭제에 실패했습니다: ${error.message}`);
+      console.error(`${ID} 번 게시글 삭제에 실패했습니다: ${error.message}`);
     });
 }
 
--- Script/API/main.mjs
+++ Script/API/main.mjs
@@ -1,4 +1,22 @@
 import * as Article from "./ArticleService.mjs";
 import * as Product from "./ProductService.mjs";
 
+// Article.getArticleList();
+// Product.getProductList();
 
+// Article.deleteArticle(57);
+// Product.deleteProduct(2);
+
+// Article.getArticle();
+// Product.getProduct();
+
+// Article.patchArticle(2, "test", "test", "test");
+// Product.patchProduct(2, "test", "test", 10, "test", ["test"], ["test"]);
+
+Article.createArticle(
+  "버즈 프로3 사도 되나요 이거?",
+  "검수상태 메롱하다던데요?",
+  "https://r1.community.samsung.com/t5/image/serverpage/image-id/8657665i5C5DAD5955B706FE/image-size/large?v=v2&px=999"
+);
+
+// Product.createProduct("string", "string", 10, "string", ["Array"], ["Array"]);
