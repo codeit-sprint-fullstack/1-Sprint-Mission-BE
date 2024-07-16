8f137a45fe3dd9339ddd6a63969915c159a18864
minseokim <claudia503@naver.com>
2024-07-16 18:38:35 +0900

    feat: mission4_ArticleService.js_createArticle

--- ArticleService.js
+++ ArticleService.js
@@ -22,3 +22,21 @@ export const getArticle = (id) => {
         .catch(error => console.error('게시글을 가져오는 중 에러 발생:', error));
 };
 
+export const createArticle = (title, content, image) => {
+    return fetch('https://sprint-mission-api.vercel.app/articles', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ title, content, image })
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`에러: ${response.statusText}`);
+            }
+            console.log('게시글이 생성되었습니다.');
+            return response.json();
+        })
+        .catch(error => console.error('게시글을 생성하는 중 에러 발생:', error));
+};
+
