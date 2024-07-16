e18577efd97e0047ce64d6444399d47ae072d7ea
minseokim <claudia503@naver.com>
2024-07-16 18:52:32 +0900

    feat: mission4_ProductService.js_createProduct

--- ProductService.js
+++ ProductService.js
@@ -22,3 +22,16 @@ export const getProduct = async (id) => {
     }
 };
 
+export const createProduct = async (name, description, price, manufacturer, tags, images) => {
+    try {
+        const response = await axios.post('https://sprint-mission-api.vercel.app/products', {
+            name, description, price, manufacturer, tags, images
+        });
+        console.log('상품이 생성되었습니다.');
+        return response.data;
+    } catch (error) {
+        console.error('상품을 생성하는 중 에러 발생:', error);
+        throw error;
+    }
+};
+
