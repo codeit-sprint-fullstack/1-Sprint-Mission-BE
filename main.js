987f9dddcb4734cb2a38d2bfe70eab6c655f1b21
minseokim <claudia503@naver.com>
2024-07-18 09:26:00 +0900

    feat: mission4_main.js_getProduct 테스트

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { getProductList } from './ProductService.js';
+import { getProduct } from './ProductService.js';
 
-getProductList()
-    .then(data => console.log('상품 목록:', data))
-    .catch(error => console.error('상품 목록을 가져오는 중 에러 발생:', error));
+getProduct(4)
+    .then(data => console.log('상품:', data))
+    .catch(error => console.error('상품을 가져오는 중 에러 발생:', error));
