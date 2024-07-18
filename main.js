e5b08cc842288a012822ff8bd479d5e271bb00bf
minseokim <claudia503@naver.com>
2024-07-18 09:14:57 +0900

    feat: mission4_main.js_createArticle 테스트

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { getArticle } from './ArticleService.js';
+import { createArticle } from './ArticleService.js';
 
-getArticle(41)
-    .then(data => console.log('게시글:', data))
-    .catch(error => console.error('게시글을 가져오는 중 에러 발생:', error));
+createArticle('새 게시글', '이것은 내용입니다', '이미지_주소')
+    .then(data => console.log('생성된 게시글:', data))
+    .catch(error => console.error('게시글을 생성하는 중 에러 발생:', error));
