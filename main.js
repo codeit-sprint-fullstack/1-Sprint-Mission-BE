dcc48312e7e6b248590cca445a98c68daa084663
minseokim <claudia503@naver.com>
2024-07-18 09:23:44 +0900

    feat: mission4_main.js_getProductList 테스트

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-import { deleteArticle } from './ArticleService.js';
+import { getProductList } from './ProductService.js';
 
-deleteArticle(128)
-    .then(() => console.log('게시글이 삭제되었습니다.'))
-    .catch(error => console.error('게시글을 삭제하는 중 에러 발생:', error));
-
+getProductList()
+    .then(data => console.log('상품 목록:', data))
+    .catch(error => console.error('상품 목록을 가져오는 중 에러 발생:', error));
