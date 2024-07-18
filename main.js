5440223939f9636ffd9490f80cda42ed0b3647fd
minseokim <claudia503@naver.com>
2024-07-18 09:05:45 +0900

    feat: mission4_main.js_getArticleList 테스트

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
+import { getArticleList } from './ArticleService.js';
 
+getArticleList()
+    .then(data => console.log('게시글 목록:', data))
+    .catch(error => console.error('게시글 목록을 가져오는 중 에러 발생:', error));
