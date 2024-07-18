ae1b87aaf8c8543ce44a30eeb52966a412ba42ee
minseokim <claudia503@naver.com>
2024-07-18 09:29:36 +0900

    feat: mission4_main.js_createProduct 테스트

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { getProduct } from './ProductService.js';
+import { createProduct } from './ProductService.js';
 
-getProduct(4)
-    .then(data => console.log('상품:', data))
-    .catch(error => console.error('상품을 가져오는 중 에러 발생:', error));
+createProduct('고디바 초콜릿', '이것은 고디바입니다', 50000, '고디바', ['초콜릿', '달달'], ['이미지_주소'])
+    .then(data => console.log('생성된 상품:', data))
+    .catch(error => console.error('상품을 생성하는 중 에러 발생:', error));
