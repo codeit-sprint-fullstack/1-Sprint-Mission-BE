09963ea5484891beea4dfd4c8e29f91ebf1a0cfd
sohee <s2h110@naver.com>
2024-07-19 17:19:15 +0900

    새로 추가할 상품/게시글 객체 추가

--- Sprint4/main.js
+++ Sprint4/main.js
@@ -1,2 +1,22 @@
 import { getProductList, getProduct, createProduct, patchProduct, deleteProduct } from "./ProductService.js";
 import { checkStatus, getArticle, getArticleList, createArticle,patchArticle,deleteArticle } from "./ArticleService.js";
+
+const newProduct = {
+    "name": "텀블러",
+    "description": "스타벅스 텀블러",
+    "price": 15000,
+    "manufacturer": "manufacturer",
+    "tags": [
+      "게임기",
+      "닌텐도"
+    ],
+    "images": [
+      "string"
+    ],
+  };
+  
+  const newArticle = {
+    "title":"레인부츠 리뷰",
+    "content":"튼튼한 레인부츠 신어본 후기",
+    "image":"string",
+  };
