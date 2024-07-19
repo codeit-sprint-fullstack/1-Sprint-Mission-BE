5ff59247e138215cc8bd612e60a3739138c07c3e
sohee <s2h110@naver.com>
2024-07-19 17:00:38 +0900

    ProductService.js, ArticleService.js 파일로부터 함수 import

---
+++ Sprint4/main.js
@@ -0,0 +1,2 @@
+import { getProductList, getProduct, createProduct, patchProduct, deleteProduct } from "./ProductService.js";
+import { checkStatus, getArticle, getArticleList, createArticle,patchArticle,deleteArticle } from "./ArticleService.js";
