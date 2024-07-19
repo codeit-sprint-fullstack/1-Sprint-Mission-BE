4c9868de9f27630c5aec5d8a20732e38e9840034
sohee <s2h110@naver.com>
2024-07-19 17:05:55 +0900

    응답상태코드 2xx인지 확인하는 함수 추가

--- Sprint4/ArticleService.js
+++ Sprint4/ArticleService.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+// 응답 상태코드가 2xx 인지 확인, 아닐경우 에러 메시지 출력
+export async function checkStatus(res) {
+    // 요청 성공적으로 처리했을 때
+    if(res.status >= 200 && res.status < 300){ 
+      const data = await res.json(); //res.json 비동기 함수 
+      return console.log('요청을 성공적으로 처리했습니다.', data);
+    }else{
+      throw new Error(`Error code: ${res.status}`);
+    }
+  }
+
 
 export async function getArticleList(page, pageSize, keyword) {
     const response = await fetch(
