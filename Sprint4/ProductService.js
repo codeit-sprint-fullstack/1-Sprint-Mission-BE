b11f69dfdfaebac13741eeccfc9d03fecfc5367a
sohee <s2h110@naver.com>
2024-07-19 17:11:43 +0900

    getProduct 함수 추가

--- Sprint4/ProductService.js
+++ Sprint4/ProductService.js
@@ -10,4 +10,16 @@ export async function getProductList(page, pageSize, keyword) {
       console.error(`Error: ${error}`);
       throw error;
     }
-  }
+}
+
+export async function getProduct(id) {
+    const URL = `https://sprint-mission-api.vercel.app/products/${id}`;
+    try {
+        const response = await axios.get(URL);
+        const data = response.data;
+        return console.log('상품 정보를 가져왔습니다.',data);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+        throw error;
+    }
+}
