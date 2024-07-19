9da0d68f13aaba4c7e38da7b50715f1740df54a8
sohee <s2h110@naver.com>
2024-07-19 17:02:38 +0900

    getArticleList 함수 추가

--- Sprint4/ArticleService.js
+++ Sprint4/ArticleService.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+
+
+export async function getArticleList(page, pageSize, keyword) {
+    const response = await fetch(
+        `https://sprint-mission-api.vercel.app/articles?page=${page}&pageSize=${pageSize}&keyword=${keyword}`)
+        .then((checkStatus))
+        .catch(error => console.error(error));
+}
