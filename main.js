67eeef69a020e950c39e923333a4674190afa391
minseokim <claudia503@naver.com>
2024-07-19 16:46:26 +0900

    feat: mission4_deleteProduct 테스트

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { patchProduct } from './ProductService.js';
+import { deleteProduct } from './ProductService.js';
 
-patchProduct(85, '나쵸', '고디바에서 나쵸로 변경되었습니다.', 2000, ['과자', '치'], ['수정된_이미지_주소'])
-    .then(data => console.log('수정된 상품:', data))
-    .catch(error => console.error('상품을 수정하는 중 에러 발생:', error));
+deleteProduct(85)
+    .then(() => console.log('상품이 삭제되었습니다.'))
+    .catch(error => console.error('상품을 삭제하는 중 에러 발생:', error));
