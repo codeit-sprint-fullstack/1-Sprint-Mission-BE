f1a245aed7643f320be9105c0a0bbfe124d8539b
mjpark-k <pmj9498@gmail.com>
2024-07-21 14:28:04 +0900

    Final modify

--- script/main.js
+++ script/main.js
@@ -23,33 +23,33 @@ const productData = {
 }
 
 // ArticleService.js 함수 호출
-// try {
-//   const articleList = await getArticleList({ pages: 1, pageSize: 10, keyword: 'sprint4 test' });
-//   console.log(articleList);
-// } catch (error) {
-//   articleErrorLog(error);
-// }
+try {
+  const articleList = await getArticleList({ pages: 1, pageSize: 10, keyword: 'sprint4 test' });
+  console.log(articleList);
+} catch (error) {
+  articleErrorLog(error);
+}
 
-// try {
-//   const article = await getArticle(171);
-//   console.log(article);
-// } catch (error) {
-//   articleErrorLog(error);
-// }
+try {
+  const article = await getArticle(171);
+  console.log(article);
+} catch (error) {
+  articleErrorLog(error);
+}
 
-// try {
-//   const createdArticle = await createArticle(articleData);
-//   console.log(createdArticle);
-// } catch (error) {
-//   articleErrorLog(error);
-// }
+try {
+  const createdArticle = await createArticle(articleData);
+  console.log(createdArticle);
+} catch (error) {
+  articleErrorLog(error);
+}
 
-// try {
-//   const updatedArticle = await patchArticle(171, articleData);
-//   console.log(updatedArticle);
-// } catch (error) {
-//   articleErrorLog(error);
-// }
+try {
+  const updatedArticle = await patchArticle(171, articleData);
+  console.log(updatedArticle);
+} catch (error) {
+  articleErrorLog(error);
+}
 
 try {
   await deleteArticle(171);
@@ -60,37 +60,37 @@ try {
 
 
 // ProductService.js 함수 호출
-// try{
-//   const getProductList = await getProductList({page: 1, pageSize: 1, keyword: '아이폰'});
-//   console.log(getProductList);
-// } catch (error) {
-//   productErrorLog(error);
-// }
+try{
+  const getProductList = await getProductList({page: 1, pageSize: 1, keyword: '아이폰'});
+  console.log(getProductList);
+} catch (error) {
+  productErrorLog(error);
+}
 
-// try{
-//   const getProduct = await getProduct(2);
-//   console.log(getProductList);
-// } catch (error) {
-//   productErrorLog(error);
-// }
+try{
+  const getProduct = await getProduct(2);
+  console.log(getProductList);
+} catch (error) {
+  productErrorLog(error);
+}
 
-// try {
-//   const createProduct = await createProduct(productData);
-//   console.log(getProductList);
-// } catch (error) {
-//   productErrorLog(error);
-// }
+try {
+  const createProduct = await createProduct(productData);
+  console.log(getProductList);
+} catch (error) {
+  productErrorLog(error);
+}
 
-// try{
-//   const patchProduct = await patchProduct(48, productData);
-//   console.log(patchProduct);
-// } catch (error) {
-//   productErrorLog(error);
-// }
+try{
+  const patchProduct = await patchProduct(48, productData);
+  console.log(patchProduct);
+} catch (error) {
+  productErrorLog(error);
+}
 
-// try {
-//   const deleteProduct = await deleteProduct(53);
-//   console.log('데이터 삭제 완료!');
-// } catch (error) {
-//   productErrorLog(error);
-// }
+try {
+  const deleteProduct = await deleteProduct(53);
+  console.log('데이터 삭제 완료!');
+} catch (error) {
+  productErrorLog(error);
+}
