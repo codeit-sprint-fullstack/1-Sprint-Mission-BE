ec52d0014546876b7ce216f13a41fafd65c42edc
mjpark-k <pmj9498@gmail.com>
2024-07-21 14:25:16 +0900

    Add contents for PR

--- script/main.js
+++ script/main.js
@@ -4,7 +4,7 @@ import { productErrorLog, getProductList, getProduct, createProduct, patchProduc
 // ArticleService.js 데이터 예시
 const articleData = {
   title: "sprint4 test",
-  content: "difficult",
+  content: "difficult, but I can do it",
   image: "image url",
 };
 
@@ -22,74 +22,75 @@ const productData = {
   ],
 }
 
-try {
-  const articleList = await getArticleList({ pages: 1, pageSize: 10, keyword: 'sprint4 test' });
-  console.log(articleList);
-} catch (error) {
-  articleErrorLog(error);
-}
+// ArticleService.js 함수 호출
+// try {
+//   const articleList = await getArticleList({ pages: 1, pageSize: 10, keyword: 'sprint4 test' });
+//   console.log(articleList);
+// } catch (error) {
+//   articleErrorLog(error);
+// }
+
+// try {
+//   const article = await getArticle(171);
+//   console.log(article);
+// } catch (error) {
+//   articleErrorLog(error);
+// }
+
+// try {
+//   const createdArticle = await createArticle(articleData);
+//   console.log(createdArticle);
+// } catch (error) {
+//   articleErrorLog(error);
+// }
+
+// try {
+//   const updatedArticle = await patchArticle(171, articleData);
+//   console.log(updatedArticle);
+// } catch (error) {
+//   articleErrorLog(error);
+// }
 
 try {
-  const article = await getArticle(57);
-  console.log(article);
-} catch (error) {
-  articleErrorLog(error);
-}
-
-try {
-  const createdArticle = await createArticle(articleData);
-  console.log(createdArticle);
-} catch (error) {
-  articleErrorLog(error);
-}
-
-try {
-  const updatedArticle = await patchArticle(16, articleData);
-  console.log(updatedArticle);
-} catch (error) {
-  articleErrorLog(error);
-}
-
-try {
-  await deleteArticle(111);
+  await deleteArticle(171);
   console.log('데이터 삭제 완료!');
 } catch (error) {
   articleErrorLog(error);
 }
 
 
-// // ProductService.js 함수 호출
-try{
-  const getProductList = await getProductList({page: 1, pageSize: 1, keyword: '아이폰'});
-  console.log(getProductList);
-} catch (error) {
-  productErrorLog(error);
-}
+// ProductService.js 함수 호출
+// try{
+//   const getProductList = await getProductList({page: 1, pageSize: 1, keyword: '아이폰'});
+//   console.log(getProductList);
+// } catch (error) {
+//   productErrorLog(error);
+// }
 
-try{
-  const getProduct = await getProduct(2);
-  console.log(getProductList);
-} catch (error) {
-  productErrorLog(error);
-}
+// try{
+//   const getProduct = await getProduct(2);
+//   console.log(getProductList);
+// } catch (error) {
+//   productErrorLog(error);
+// }
 
-try {
-  const createProduct = await createProduct(productData);
-  console.log(getProductList);
-} catch (error) {
-  productErrorLog(error);
-}
+// try {
+//   const createProduct = await createProduct(productData);
+//   console.log(getProductList);
+// } catch (error) {
+//   productErrorLog(error);
+// }
 
-try{
-  const patchProduct = await patchProduct(48, productData);
-  console.log(patchProduct);
-} catch (error) {
-  productErrorLog(error);
-}
+// try{
+//   const patchProduct = await patchProduct(48, productData);
+//   console.log(patchProduct);
+// } catch (error) {
+//   productErrorLog(error);
+// }
 
-try {
-  const deleteProduct = await deleteProduct(53);
-  console.log('데이터 삭제 완료!');
-} catch (error) {
-  productErrorLog(error);
-}
+// try {
+//   const deleteProduct = await deleteProduct(53);
+//   console.log('데이터 삭제 완료!');
+// } catch (error) {
+//   productErrorLog(error);
+// }
