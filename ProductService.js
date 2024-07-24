6a3501b6c727475218871d297334d63d864b6aba
jongmin <charlieko123@github.com>
2024-07-25 08:57:44 +0900

    add Destruncturing assignment

--- ProductService.js
+++ ProductService.js
@@ -1,76 +1,75 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://sprint-mission-api.vercel.app',
+  baseURL: "https://sprint-mission-api.vercel.app",
 });
 
-export async function getProductList({page = 1, pageSize = 10, keyword = ''} = {}){
+export async function getProductList({
+  page = 1,
+  pageSize = 10,
+  keyword = "",
+} = {}) {
   const params = {
     page,
     pageSize,
     keyword,
   };
-  try{
-    const res = await instance.get('/products', { params });
-    console.log(res.data);
-  } catch(error) {
-    if(error.response){
+  try {
+    const { data } = await instance.get("/products", { params });
+    console.log(data);
+  } catch (error) {
+    if (error.response) {
       console.log(error.response.status);
       console.log(error.response.data);
-    }
-    else console.log('리퀘스트가 실패했습니다.');
+    } else console.log("리퀘스트가 실패했습니다.");
   }
 }
 
-export async function getProduct(id){
-  try{
-    const res = await instance.get(`/products/${id}`);
-    console.log(res.data);
-  } catch(error) {
-    if(error.response){
+export async function getProduct(id) {
+  try {
+    const { data } = await instance.get(`/products/${id}`);
+    console.log(data);
+  } catch (error) {
+    if (error.response) {
       console.log(error.response.status);
       console.log(error.response.data);
-    }
-    else console.log('리퀘스트가 실패했습니다.');
+    } else console.log("리퀘스트가 실패했습니다.");
   }
 }
 
-export async function createProduct(productContent){
-  try{
-    const res = await instance.post('/products', productContent);
-    console.log(res.data);
-  } catch(error) {
-    if(error.response){
+export async function createProduct(productContent) {
+  try {
+    const { data } = await instance.post("/products", productContent);
+    console.log(data);
+  } catch (error) {
+    if (error.response) {
       console.log(error.response.status);
       console.log(error.response.data);
-    }
-    else console.log('리퀘스트가 실패했습니다.');
+    } else console.log("리퀘스트가 실패했습니다.");
   }
 }
 
-export async function patchProduct(id, updateContent){
-  try{
-    const res = await instance.patch(`/products/${id}`, updateContent);
-    console.log(res.data);
-  } catch(error) {
-    if(error.response){
+export async function patchProduct(id, updateContent) {
+  try {
+    const { data } = await instance.patch(`/products/${id}`, updateContent);
+    console.log(data);
+  } catch (error) {
+    if (error.response) {
       console.log(error.response.status);
       console.log(error.response.data);
-    }
-    else console.log('리퀘스트가 실패했습니다.');
+    } else console.log("리퀘스트가 실패했습니다.");
   }
 }
 
-export async function deleteProduct(id){
-  try{
-    const res = await instance.delete(`/products/${id}`);
-    console.log(res.data);
+export async function deleteProduct(id) {
+  try {
+    const { data } = await instance.delete(`/products/${id}`);
+    console.log(data);
     console.log("delete successful");
-  } catch(error) {
-    if(error.response){
+  } catch (error) {
+    if (error.response) {
       console.log(error.response.status);
       console.log(error.response.data);
-    }
-    else console.log('리퀘스트가 실패했습니다.');
+    } else console.log("리퀘스트가 실패했습니다.");
   }
 }
