7c946196091b2b7f5f7a5713e8ce52fa508091f7
sohee <s2h110@naver.com>
2024-07-30 11:36:19 +0900

    BestProducts.js 파일 추가

---
+++ src/components/BestProducts.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { getProductList } from '../api/api';
+import ProductListItem from './ProductListItem';
+
+function BestProducts() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchBestProducts = async () => {
+      try {
+        const response = await getProductList({ order: 'favoriteCount', offset: 0, limit: 4 });
+        console.log('응답 데이터:', response); 
+        
+        const sortedProducts = response.list.sort((a, b) => b.favoriteCount - a.favoriteCount);
+        setProducts(sortedProducts);
+      } catch (error) {
+        console.error('베스트 상품을 불러오는데 실패했습니다.', error);
+      }
+    };
+
+    fetchBestProducts();
+  }, []);
+
+  const topProducts = products.slice(0, 4);
+
+  return (
+    <section className="best-products">
+      <div className="bestProductList">
+        {topProducts.map(product => (
+          <ProductListItem key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default BestProducts;
