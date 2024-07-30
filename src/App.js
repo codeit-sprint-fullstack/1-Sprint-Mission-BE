d89708b2bc4c275be8933358f204e2d78b65f0ce
sohee <s2h110@naver.com>
2024-07-30 13:59:40 +0900

    상품리스트 정렬 함수 및 로딩 오류 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import searchIcon from './assets/images/ic_search.png';
 import Header from './components/Header';
 import BestProducts from './components/BestProducts';
 import ProductList from './components/ProductList';
+import { getProductList } from './api/api';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [order, setOrder] = useState('createdAt');
+  const [loadingError, setLoadingError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await getProductList({ order });
+        setProducts(response.list || []);
+      } catch (error) {
+        setLoadingError('상품을 불러오는 데 실패했습니다.');
+      }
+    };
+
+    fetchProducts();
+  }, [order]);
+
+  const handleOrderChange = (event) => {
+    setOrder(event.target.value);
+  };
+
+
+  // 정렬 함수
+  const sortedProducts = [...products].sort((a, b) => {
+    if (order === 'createdAt') {
+      return new Date(b.createdAt) - new Date(a.createdAt);
+    } else if (order === 'favoriteCount') {
+      return b.favoriteCount - a.favoriteCount;
+    }
+    return 0;
+  });
 
   return (
     <div className='App'>
@@ -21,18 +53,20 @@ function App() {
               placeholder="검색할 상품을 입력해주세요" 
               className="search-input" 
               style={{ backgroundImage: `url(${searchIcon})` }}
-              />
-              <button className="search-button">검색</button>
-              <button className='addProductBotton'>상품 등록하기</button>
-              <select className="sortDropDown">
-                <option value="createdAt">최신순</option>
-                <option value="favoriteCount">좋아요순</option>
-              </select>
+            />
+            <button className="search-button">검색</button>
+            <button className='addProductBotton'>상품 등록하기</button>
+            <select className="sortDropDown" onChange={handleOrderChange} value={order}>
+              <option value="createdAt">최신순</option>
+              <option value="favoriteCount">좋아요순</option>
+            </select>
           </div>
         </div>
-        <ProductList />
+        {loadingError && <p>{loadingError}</p>}
+        <ProductList products={sortedProducts}/>
       </main>
     </div>
   );
 }
+
 export default App;
