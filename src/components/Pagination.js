a7967048a64e46b4adabe411a559f5d908f62aa2
sohee <s2h110@naver.com>
2024-07-30 14:36:29 +0900

    페이지네이션 페이지 버튼 기능 추가

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import './Pagination.css';
 
-const Pagination = ({currentPage, totalPages}) => {
+const Pagination = ({ currentPage, totalPages, onPageChange, hasNext }) => {
     
-    // 페이지 범위 계산
-    const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1;
-    const endPage = Math.min(startPage + 4, totalPages);
-  
+  // 페이지 범위 계산
+  const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1;
+  const endPage = Math.min(startPage + 4, totalPages);
+
   return (
     <div className="pagination">
       <div className='pageButtons'>
         <button 
-          className="leftPageButton"
+          className="leftPageButton" 
           onClick={() => onPageChange(currentPage - 1)} 
-          disabled={currentPage === 1} 
+          disabled={currentPage === 1}
         >
           &lt;
         </button>
-          <button>숫자
+        {Array.from({ length: endPage - startPage + 1 }, (_, index) => (
+          <button 
+            key={startPage + index}
+            onClick={() => onPageChange(startPage + index)}
+            className={startPage + index === currentPage ? 'active' : ''}
+          >
+            {startPage + index}
           </button>
+        ))}
         <button 
           className='rightPageButton' 
           onClick={() => onPageChange(currentPage + 1)} 
