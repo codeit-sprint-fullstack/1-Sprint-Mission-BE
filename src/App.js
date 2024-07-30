cb9ade41f4a755572a934a7c580e89dc61c12745
sohee <s2h110@naver.com>
2024-07-30 13:13:41 +0900

    판매중인 상품 및 검색 input 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import searchIcon from './assets/images/ic_search.png';
 import Header from './components/Header';
 import BestProducts from './components/BestProducts';
 import './App.css';
@@ -11,6 +12,17 @@ function App() {
       <main>
         <h2>베스트 상품</h2>
         <BestProducts />
+        <div className='SaleProductNav'>
+          <h2>판매 중인 상품</h2>
+          <div className='inputBtDrop'>
+            <input 
+              type="text" 
+              placeholder="검색할 상품을 입력해주세요" 
+              className="search-input" 
+              style={{ backgroundImage: `url(${searchIcon})` }}
+              />
+          </div>
+        </div>
       </main>
     </div>
   );
