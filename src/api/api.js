f8240d2d9d9c068f3a41873e5e9957897c5e9b23
sohee <s2h110@naver.com>
2024-07-30 11:32:50 +0900

    상품 리스트를 가져오기 위한 getProductLis 함수 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,23 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://panda-market-api.vercel.app/products';
+
+export async function getProductList({
+  order = 'createdAt',
+  cursor = null,
+  limit = 3,
+} = {}) {
+try {
+  const query = new URLSearchParams({
+    order,
+    limit,
+    cursor: cursor || ''
+  }).toString();
+  const response = await axios.get(`${BASE_URL}?${query}`);
+  const body = response.data;
+  console.log('API 응답 데이터:', body);
+  return body;
+} catch (error) {
+  throw new Error('상품을 불러오는데 실패했습니다');
+}
+}
