bb97afc9a69d68a5d0258b828961edcc01a158c7
sohee <s2h110@naver.com>
2024-07-30 13:07:14 +0900

    Refactor App.js to include Header and BestProducts components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import BestProducts from './components/BestProducts';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <main>
+        <h2>베스트 상품</h2>
+        <BestProducts />
+      </main>
     </div>
   );
 }
-
 export default App;
