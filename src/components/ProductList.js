753914b9c9244130227783e9b218e9e5521168ca
sohee <s2h110@naver.com>
2024-07-30 11:39:35 +0900

    판매중인 상품 리스트인 ProductList.js 파일 추가

---
+++ src/components/ProductList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ProductListItem from './ProductListItem';
+
+
+function ProductList({ products }) {
+    return (
+      <section className="sale-products">
+        <ul className="product-list">
+          {products.map((product) => (
+            <ProductListItem key={product.id} product={product} />
+        ))}
+        </ul>
+      </section>
+    );
+}
+
+export default ProductList;
