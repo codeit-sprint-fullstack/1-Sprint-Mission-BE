4df1a56e464f546606dee4aef12d23c1ef3eff51
sohee <s2h110@naver.com>
2024-07-30 14:30:45 +0900

    페이지 범위를 계산하기 위한 코드  추가

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({currentPage, totalPages}) => {
     
+    // 페이지 범위 계산
+    const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1;
+    const endPage = Math.min(startPage + 4, totalPages);
+  
   return (
     <div className="pagination">
       <div className='pageButtons'>
