f2fbc457c20545c9fa3d217016b3407ecdfd14f2
jongmin <charlieko123@github.com>
2024-08-05 16:38:53 +0900

    add post

--- src/app.js
+++ src/app.js
@@ -1,5 +1,45 @@
 import express from "express";
 import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+dotenv.config();
 import Product from "./models/product.js";
 
 const app = express();
+
+app.use(express.json());
+
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === "ValidationError") {
+        res.status(400).send({ message: e.message });
+      } else if (e.name === "CastError") {
+        res.status(404).send({ message: "Cannot find given id." });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
+// 상품 목록 조회
+// app.get("/products", asyncHandler(async (req, res) => {
+//   const sort = req.query.sort;
+// }))
+// 상품 상세 조회
+
+// 상품 등록
+app.post(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const newProduct = await Product.create(req.body);
+
+    res.status(201).send(newProduct);
+  })
+);
+
+app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to DB"));
