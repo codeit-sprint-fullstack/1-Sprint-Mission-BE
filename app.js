c6f4a5c19868699c276b4ed5242345641edd5e2f
minsu <a2238737@gmail.com>
2024-08-05 17:29:55 +0900

    feat: API 생성

---
+++ app.js
@@ -0,0 +1,79 @@
+import express from "express";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import Product from "./models/Product.js";
+import cors from "cors";
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL).then(() => {
+  console.log("Database connected");
+});
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      console.log(e.name);
+      console.log(e.message);
+    }
+  };
+}
+
+app.get(
+  "/product",
+  asyncHandler(async (req, res) => {
+    try {
+      const product = await Product.find();
+      res.send(product);
+    } catch (e) {
+      res.status(500).send({ message: e.message });
+    }
+  })
+);
+
+app.post(
+  "/product",
+  asyncHandler(async (req, res) => {
+    const product = await Product.create(req.body);
+    res.status(201).send(product);
+  })
+);
+
+app.patch(
+  "/product/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (product) {
+      Object.keys(req.body).forEach((key) => {
+        product[key] = req.body[key];
+      });
+      await product.save();
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Product not found" });
+    }
+  })
+);
+
+app.delete(
+  "/product/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Product not found" });
+    }
+  })
+);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started on port 3000");
+});
---
+++ models/Product.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: 10,
+    },
+    description: {
+      type: String,
+      required: true,
+      minLength: 10,
+      maxLength: 100,
+    },
+    price: {
+      type: Number,
+      required: true,
+      minLength: 1,
+    },
+    tag: {
+      type: String,
+      required: true,
+      maxLength: 5,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+
+export default Product;
