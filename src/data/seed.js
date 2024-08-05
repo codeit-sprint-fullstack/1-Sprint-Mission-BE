a93e386c09215a87c7681e1c93c4da503c3d4f05
jongmin <charlieko123@github.com>
2024-08-05 15:03:00 +0900

    add seed.js and dotenv

--- src/data/seed.js
+++ src/data/seed.js
@@ -1,2 +1,12 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
+import Product from "../models/product.js";
+import data from "./mockData.js";
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL);
+
+await Product.deleteMany({});
+await Product.insertMany(data);
+
+mongoose.connection.close();
