9f1ceca149ccae8a29a2505f96a16f20ab2be3f6
S2OHEE <s2h110@naver.com>
2024-08-06 13:47:42 +0900

    dotenv 추가 및 .env 파일을 통한 환경 변수 설정

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+import dotenv from 'dotenv';
+dotenv.config(); // 환경 변수 설정
+
 import express from 'express';
-import { DATABASE_URL } from './env.js';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(express.json());
 
+console.log('Database_URL:', process.env.DATABASE_URL);
+
 // MongoDB 연결
-mongoose.connect(DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-app.listen(3000, () => console.log('Server Started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
