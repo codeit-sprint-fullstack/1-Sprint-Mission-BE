955a847bbe853a6ebd274e0a4d1ce59b69d26046
S2OHEE <s2h110@naver.com>
2024-08-06 13:37:01 +0900

    DATABASE_URL import 및 mongoDB 연결

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import { DATABASE_URL } from './env.js';
 
 const app = express();
 app.use(express.json());
 
+// MongoDB 연결
+mongoose.connect(DATABASE_URL)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
 
 app.listen(3000, () => console.log('Server Started'));
