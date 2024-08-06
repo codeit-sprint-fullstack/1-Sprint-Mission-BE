a4d3e9b8748c5f3b7dc21092f7c3c96f2fdb4164
S2OHEE <s2h110@naver.com>
2024-08-06 14:01:16 +0900

    seed.js 파일 추가 및 환경 변수 로드 설정

---
+++ data/seed.js
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { Data } from './mock.js';
+import Product from '../models/Product.js';
+
+dotenv.config(); // .env 파일에서 환경 변수를 로드
