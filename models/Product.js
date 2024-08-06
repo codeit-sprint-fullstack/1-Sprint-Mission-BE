4062f3bffb7bbc6a7ca53625ba3f1728232f97fe
S2OHEE <s2h110@naver.com>
2024-08-06 13:16:18 +0900

    Product 스키마 및 모델 생성

---
+++ models/Product.js
@@ -0,0 +1,29 @@
+// Product 스키마
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+},
+{
+  timestamps: true,
+}
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
