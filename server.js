6ee55215c67d7305a00e218721f1dc2c5308dde3
S2OHEE <s2h110@naver.com>
2024-08-06 14:16:25 +0900

    상품 상세 조회 API 추가

--- server.js
+++ server.js
@@ -14,4 +14,21 @@ mongoose.connect(process.env.DATABASE_URL)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
-app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
+// 상품 상세 조회
+app.get('/api/products/:id', async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const product = await Product.findById(id);
+      if (!product) {
+        return res.status(404).send({ message: '상품을 찾을 수 없습니다.' });
+      }
+  
+      res.status(200).send(product);
+    } catch (error) {
+      console.error('상품 상세 조회 중 오류 발생:', error);
+      res.status(500).send({ error: '상품을 불러오는 데 실패했습니다' });
+    }
+  });
+  
+  app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
