0f5d665636ecc66ba703d82a642792a48d106c37
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-08-07 16:47:48 +0900

    chore: products.js 경로 수정

--- routes/products.js
+++ routes/products.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/", getProducts);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.put("/", updateProduct);
+router.delete("/", deleteProduct);
 
 export default router;
