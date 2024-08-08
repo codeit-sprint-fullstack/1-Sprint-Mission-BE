c2c8554f8bb1958f8095fc99f418519b296ba4f7
mjpark-k <pmj9498@gmail.com>
2024-08-08 14:46:16 +0900

    CRUD 추가 및 에러 처리

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ mongoose.connect(DATABASE_URL).then(() => console.log("Connected to DB"));
 const app = express();
 app.use(express.json());
 
+// 에러 처리 함수
 function asyncHandler(handler) {
   return async function (req, res) {
     try {
@@ -22,5 +23,70 @@ function asyncHandler(handler) {
     }
   };
 }
+
+// CRUD 연산
+app.post(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const newProduct = await Product.create(req.body);
+    res.status(201).send(newProduct);
+  })
+);
+
+app.get(
+  "/products",
+  asyncHandler(async (req, res) => {
+    const sort = req.query.sort;
+    const count = Number(req.query.count) || 0;
+
+    const sortOption = { createdAt: sort === "recent" ? "asc" : "desc" };
+    const products = await Product.find().sort(sortOption).limit(count);
+
+    res.send(products);
+  })
+);
+
+app.get(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Cannot find given id. " });
+    }
+  })
+);
+
+app.patch(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (product) {
+      Object.keys(req.body).forEach((key) => {
+        product[key] = req.body[key];
+      });
+      await product.save();
+      res.send(product);
+    } else {
+      res.status(404).send({ message: "Cannot find given id. " });
+    }
+  })
+);
+
+app.delete(
+  "/products/:id",
+  asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const product = await Product.findByIdAndDelete(id);
+    if (product) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).send({ message: "Cannot find given id. " });
+    }
+  })
+);
 
 app.listen(3000, () => console.log("Server Started"));
