b54248a3206bae1fc0e0a04911347b6caddfa6a0
minsu <a2238737@gmail.com>
2024-08-09 16:37:40 +0900

    URL 변경

--- main.js
+++ main.js
@@ -3,10 +3,3 @@ import express from "express";
 
 const app = express();
 const prisma = new PrismaClient();
-const port = 3000;
-
-app.get("/", async (req, res) => {
-  const articles = await prisma.article.findMany();
-  console.log("test");
-  res.send(articles);
-});
