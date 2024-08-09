3c3e0bd8fc2f4d491920dfa5eb4a381ff7b81816
minsu <a2238737@gmail.com>
2024-08-09 16:21:08 +0900

    fix

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-import * as article from "./article/article.js";
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+
+const app = express();
+const prisma = new PrismaClient();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  const articles = await prisma.article.findMany();
+  console.log("test");
+  res.send(articles);
+});
