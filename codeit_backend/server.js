0e394490a9c1ce0b5f1132958ffc471356aef973
minseokim <claudia503@naver.com>
2024-08-09 10:24:19 +0900

    feat: mission6_server.js MongoDB 연결확인

--- codeit_backend/server.js
+++ codeit_backend/server.js
@@ -16,7 +16,12 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
 });
+
 
 app.use('/api', productRoutes);
 
