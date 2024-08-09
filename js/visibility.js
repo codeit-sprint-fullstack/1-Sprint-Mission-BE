0a34cbd448e1ddeb46b08db77d3f24cbc6a16235
minseokim <claudia503@naver.com>
2024-08-09 23:05:43 +0900

    feat: mission6 불필요한 파일 정리

--- js/visibility.js
+++
@@ -1,6 +0,0 @@
-function togglePasswordVisibility(fieldId, toggleElement) {
-    const field = document.getElementById(fieldId);
-    const isPasswordVisible = field.type === 'text';
-    field.type = isPasswordVisible ? 'password' : 'text';
-    toggleElement.querySelector('img').src = isPasswordVisible ? '/image/invisible.svg' : '/image/visible.svg';
-}
