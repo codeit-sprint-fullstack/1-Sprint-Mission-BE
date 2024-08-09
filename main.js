5d84d8ceb630f66cfa166c7fad7297facd96ae37
minsu <a2238737@gmail.com>
2024-08-09 16:56:50 +0900

    fix

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: process.env.DATABASE_URL,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 const prisma = new PrismaClient();
