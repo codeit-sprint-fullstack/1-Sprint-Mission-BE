b98d167e54ab9506f3f61351e776863dca0bdb68
jongmin <charlieko123@github.com>
2024-08-09 14:16:43 +0900

    add post

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
-import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import cors from "cors";
-import Product from "./models/product.js";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
 const app = express();
+const prisma = new PrismaClient();
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,19 +16,24 @@ function asyncHandler(handler) {
     try {
       await handler(req, res);
     } catch (e) {
-      if (e.name === "ValidationError") {
-        res.status(400).send({ message: e.message });
-      } else if (e.name === "CastError") {
-        res.status(404).send({ message: "Cannot find given id." });
-      } else {
-        res.status(500).send({ message: e.message });
-      }
+      res.status(500).send({ message: e.message });
     }
   };
 }
 app.get("/", (req, res) => {
   res.send("default path");
 });
+// 게시글 등록
+app.post(
+  "articles",
+  asyncHandler(async (req, res) => {
+    const article = await prisma.article.create({
+      data: req.body,
+    });
+    res.status(201).send(article);
+  })
+);
+
 // 상품 목록 조회
 app.get(
   "/products",
@@ -67,15 +73,6 @@ app.get(
     } else {
       res.status(404).send({ message: "cannot find given id." });
     }
-  })
-);
-// 상품 등록
-app.post(
-  "/products",
-  asyncHandler(async (req, res) => {
-    const newProduct = await Product.create(req.body);
-
-    res.status(201).send(newProduct);
   })
 );
 // 상품 수정
