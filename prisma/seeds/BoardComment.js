7bf551c9b615edb6af9c85678948f12c85c8fb37
S2OHEE <s2h110@naver.com>
2024-08-10 18:06:31 +0900

    feat: 자유게시판 Comment 시드 데이터 추가

---
+++ prisma/seeds/BoardComment.js
@@ -0,0 +1,42 @@
+export const BoardComments = [
+  {
+      content: "제 생각에는 가격을 낮춰야할거같습니다.",
+      postId: 123 
+  },
+  {
+      content: "배송이 얼마나 걸렸나요?",
+      postId: 125
+  },
+  {
+      content: "다른 사람들 의견도 듣고 싶어요.",
+      postId: 123
+  },
+  {
+      content: "해결책을 제안해주셔서 감사합니다!",
+      postId: 126
+  },
+  {
+      content: "혹시 사용기간이 어떻게 되실까요?",
+      postId: 130
+  },
+  {
+      content: "직접 택배사에 연락 해보셔야할거같아요",
+      postId: 131
+  },
+  {
+      content: "상품에 대한 상세 설명이 부족해요.",
+      postId: 132
+  },
+  {
+      content: "가격을 더 낮추실 생각은 없으신가요?",
+      postId: 133
+  },
+  {
+      content: "직거래 가능할까요?",
+      postId: 208
+  },
+  {
+      content: "하자가 있는 부분의 사진이 필요할거같습니다.",
+      postId: 209
+  }
+];
