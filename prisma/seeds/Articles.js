f3e3ded2ca6bdeb7da0cadc25cc81b0260c0d931
S2OHEE <s2h110@naver.com>
2024-08-10 16:32:43 +0900

    게시글 시드 데이터 추가

---
+++ prisma/seeds/Articles.js
@@ -0,0 +1,62 @@
+export const articleData = [
+    {
+      id: 123,
+      title: "텀블러",
+      content: "텀블러구매",
+      likeCount: 0
+    },
+    {
+      id: 125,
+      title: "새 게시글",
+      content: "이것은 내용입니다",
+      likeCount: 0
+    },
+    {
+      id: 126,
+      title: "새 게시글",
+      content: "안녕하세요",
+      likeCount: 0
+    },
+    {
+      id: 130,
+      title: "테스트",
+      content: "테스트입니다",
+      likeCount: 0
+    },
+    {
+      id: 131,
+      title: "안녕하세요",
+      content: "만나서 반갑습니다.",
+      likeCount: 0
+    },
+    {
+      id: 132,
+      title: "string",
+      content: "string",
+      likeCount: 0
+    },
+    {
+      id: 133,
+      title: "새 게시글",
+      content: "이것은 내용입니다",
+      likeCount: 0
+    },
+    {
+      id: 208,
+      title: "감자",
+      content: "포슬포슬하고 맛있어요. 배송상태도 아주 훌륭합니다.",
+      likeCount: 0
+    },
+    {
+      id: 209,
+      title: "피데기",
+      content: "쫀득하니 맛있습니다. 맥주안주로 아주 훌륭합니다.",
+      likeCount: 0
+    },
+    {
+      id: 210,
+      title: "랍스타",
+      content: "정말 살아있는 랍스타가 배송되네요. 아주 신선합니다.",
+      likeCount: 5
+    }
+  ];
