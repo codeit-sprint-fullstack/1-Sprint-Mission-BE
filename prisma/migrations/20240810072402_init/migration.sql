a92bf333f0da90a6347df74b7229280de7ee4af5
S2OHEE <s2h110@naver.com>
2024-08-10 16:25:08 +0900

    Prisma 마이그레이션 초기화

---
+++ prisma/migrations/20240810072402_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "likeCount" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Article_title_key" ON "Article"("title");
