8471fb2acf4bed9a76cdbec88364c9d2b9ac3901
wxy0415 <qkrtkd727@gmail.com>
2024-08-11 19:16:59 +0900

    백엔드 api 일부 수정했습니다.

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function asyncHandler(handler) {
 }
 
 app.get('/products', async (req, res) => {
-  const { sort = 'recent', offset = 0, limit = 10, search = '' } = req.query;
+  const { sort = 'recent', offset = 0, search = '' } = req.query;
 
   const sortOption = {
     createdAt: sort === 'recent' ? 'desc' : 'asc',
@@ -40,11 +40,7 @@ app.get('/products', async (req, res) => {
     $or: [{ name: { $regex: search, $options: 'i' } }, { description: { $regex: search, $options: 'i' } }],
   };
 
-  const products = await Product.find(searchQuery)
-    .sort(sortOption)
-    .skip(Number(offset))
-    .limit(Number(limit))
-    .select('_id name price createdAt');
+  const products = await Product.find(searchQuery).sort(sortOption).skip(Number(offset)).select('_id name price createdAt');
 
   res.send(products);
 });
