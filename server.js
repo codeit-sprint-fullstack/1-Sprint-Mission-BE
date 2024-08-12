0373c85a114ada6bfb66fd7417aac0c89d32de92
S2OHEE <s2h110@naver.com>
2024-08-12 11:16:47 +0900

    refactor: 기본 서버 포트를 8000으로 변경

--- server.js
+++ server.js
@@ -127,4 +127,4 @@ app.delete('/api/products/:id', async (req, res) => {
     }
   });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
+app.listen(process.env.PORT || 8000, () => console.log('Server Started'));
