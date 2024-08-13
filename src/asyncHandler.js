6451c40c83aa3620f3381ff0b898ee9785046bf3
jongmin <charlieko123@github.com>
2024-08-13 23:50:00 +0900

    divide asyncHandler, router

---
+++ src/asyncHandler.js
@@ -0,0 +1,17 @@
+function asyncHandler(handler) {
+  return async function (req, res) {
+    try {
+      await handler(req, res);
+    } catch (e) {
+      if (e.name === "ValidationError") {
+        res.status(400).send({ message: e.message });
+      } else if (e.name === "CastError") {
+        res.status(404).send({ message: "Cannot find given id." });
+      } else {
+        res.status(500).send({ message: e.message });
+      }
+    }
+  };
+}
+
+export default asyncHandler;
