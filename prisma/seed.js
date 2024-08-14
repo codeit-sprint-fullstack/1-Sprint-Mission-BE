4f557a6bf2fdaf696c8321bc5051b9f463618348
mjpark <pmj9498@gmail.com>
2024-08-14 17:45:37 +0900

    feat: 시딩하기

---
+++ prisma/seed.js
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { PRODUCTS } from "./mock.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.product.deleteMany();
+
+  await prisma.product.createMany({
+    data: PRODUCTS,
+    skipDuplicates: true,
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
