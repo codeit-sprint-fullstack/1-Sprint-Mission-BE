76d88d8de5e13c45b627560b2ea5867c871077cd
mini-1018 <anjaemin1018@gmail.com>
2024-08-15 16:42:20 +0900

    상품, 상품댓글, 댓글ondelete 작성

---
+++ prisma/migrations/20240815072427_temporary_comment_product/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Art" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Art_pkey" PRIMARY KEY ("id")
+);
