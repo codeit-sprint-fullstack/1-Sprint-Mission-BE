99346e432e4a20ab5ebe0da0ba58bc628dbada92
minseokim <claudia503@naver.com>
2024-08-18 02:11:28 +0900

    feat: mission7 Comment 모델 추가 및 Product, Article과의 관계 설정을 위한 Prisma 마이그레이션 생성

---
+++ prisma/migrations/20240817171020_add_comment/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "productId" INTEGER,
+    "articleId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article"("id") ON DELETE CASCADE ON UPDATE CASCADE;
