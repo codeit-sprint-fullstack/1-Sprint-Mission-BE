9b223e04752e2718e88d5a8df945649a66e05229
minseokim <claudia503@naver.com>
2024-08-18 02:05:57 +0900

    feat: mission7 Product 모델 추가를 위한 Prisma 마이그레이션 생성

---
+++ prisma/migrations/20240817165946_add_product/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
