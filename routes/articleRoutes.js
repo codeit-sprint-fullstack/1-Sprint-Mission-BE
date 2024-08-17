4471f622765b6095ce8f4811bacd2b2589b2f92a
minseokim <claudia503@naver.com>
2024-08-18 02:45:51 +0900

    feat: mission7_articleRoutes.js Article 관련 CRUD API 라우트 설정

---
+++ routes/articleRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const {
+  createArticle,
+  getArticles,
+  getArticleById,
+  updateArticle,
+  deleteArticle,
+} = require('../controllers/articleController');
+
+router.post('/articles', createArticle);
+router.get('/articles', getArticles);
+router.get('/articles/:id', getArticleById);
+router.patch('/articles/:id', updateArticle);
+router.delete('/articles/:id', deleteArticle);
+
+module.exports = router;
+
