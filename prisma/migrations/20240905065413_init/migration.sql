b0599524a67bbc04e9847ed812352e151fe6f9a9
juyeongeun <80696592+juyeongeun@users.noreply.github.com>
2024-09-05 15:54:22 +0900

    chore: db 서버 재생성

---
+++ prisma/migrations/20240905065413_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Article" ADD COLUMN     "favorite" INTEGER NOT NULL DEFAULT 0;
--- service/articleService.js
+++ service/articleService.js
@@ -24,21 +24,32 @@ export const getArticleById = asyncHandler(async (req, res) => {
 });
 
 export const getArticles = asyncHandler(async (req, res) => {
-  const { page = 1, size = 10, search = "" } = req.query;
+  const { page = 1, size = 10, search = "", sort = "createdAt" } = req.query;
   const offset = (page - 1) * size;
+
+  // 정렬 기준 설정: 최신순('createdAt') 또는 좋아요순('favorite')
+  let orderBy;
+  if (sort === "favorite") {
+    orderBy = { favorite: "desc" }; // 좋아요순으로 정렬
+  } else {
+    orderBy = { createdAt: "desc" }; // 최신순으로 정렬 (기본값)
+  }
+
   const articles = await prisma.article.findMany({
     where: {
       OR: [{ title: { contains: search } }, { content: { contains: search } }],
     },
     skip: parseInt(offset),
     take: parseInt(size),
-    orderBy: { createdAt: "desc" },
+    orderBy, // 동적 정렬 적용
   });
+
   const total = await prisma.article.count({
     where: {
       OR: [{ title: { contains: search } }, { content: { contains: search } }],
     },
   });
+
   res.status(200).json({
     total,
     pages: Math.ceil(total / size),
