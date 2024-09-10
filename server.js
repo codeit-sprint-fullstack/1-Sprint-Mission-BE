95f6d14701c41e072582d43600a4bedc0056eee2
minseokim <claudia503@naver.com>
2024-09-11 08:34:05 +0900

    fix: api 경로 재설정

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// server.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -8,20 +9,17 @@ const commentRoutes = require('./routes/commentRoutes');
 dotenv.config();
 
 const app = express();
-
-
 app.use(cors({
-  origin: 'https://ibaomarket.netlify.app'
+  origin: 'https://ibaomarket.netlify.app',
 }));
-
 app.use(express.json());
 
-app.use('/api', productRoutes);
-app.use('/api', articleRoutes);
-app.use('/api', commentRoutes);
+app.use('/api/products', productRoutes);  // /api/products 경로
+app.use('/api/articles', articleRoutes);  // /api/articles 경로
+app.use('/api/comments', commentRoutes);  // /api/comments 경로
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+  console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
 });
 
