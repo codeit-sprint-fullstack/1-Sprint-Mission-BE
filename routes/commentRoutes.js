7d0a07dfdc18b3ec8cca4becde7765ce147371c5
minseokim <claudia503@naver.com>
2024-09-11 11:55:58 +0900

    fix: commentRoutes.js 댓글 routes 파일에 대해 중복된 경로를 제거하고 상대 경로로 수정

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -9,12 +9,12 @@ const {
   getArticleComments,
 } = require('../controllers/commentController');
 
-// 중복된 경로 제거
-router.post('/products/:productId/comments', createProductComment);
-router.post('/articles/:articleId/comments', createArticleComment);
+// 중복된 경로 제거하고 상대 경로로 수정
+router.post('/:productId/comments', createProductComment);  // /api/products/:productId/comments
+router.post('/:articleId/comments', createArticleComment);  // /api/articles/:articleId/comments
 router.patch('/:id', updateComment);
 router.delete('/:id', deleteComment);
-router.get('/products/:productId/comments', getProductComments);
-router.get('/articles/:articleId/comments', getArticleComments);
+router.get('/:productId/comments', getProductComments);  // /api/products/:productId/comments
+router.get('/:articleId/comments', getArticleComments);  // /api/articles/:articleId/comments
 
 module.exports = router;
