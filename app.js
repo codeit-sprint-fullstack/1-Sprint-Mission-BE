5f62f143b29f21b12dbccf42410b608c72e4c514
mini-1018 <anjaemin1018@gmail.com>
2024-09-16 22:10:04 +0900

    fix : cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   })
 );
