2c022791f15c6510c40c131467d28b4e21ba7cb6
mozzi34 <160555885+mozzi34@users.noreply.github.com>
2024-10-01 19:31:25 +0900

    이미지 업로드 관련 수정

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-import multer from 'multer';
-import path from 'path';
 
 import userRoute from './src/routes/users.js';
 import freeboardRoute from './src/routes/freeboard.js';
@@ -11,23 +9,13 @@ import articleRoute from './src/routes/articles.js';
 import commentRoute from './src/routes/comments.js';
 import favoriteRoute from './src/routes/favorite.js';
 
+import multer from 'multer';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// Multer 설정
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'src/uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
 
 app.get('/', (req, res) => {
   res.send('default path');
@@ -39,15 +27,5 @@ app.use('/fleamarket', fleamarketRoute);
 app.use('/comments', commentRoute);
 app.use('/users', userRoute);
 app.use('/favorite', favoriteRoute);
-
-app.post('/files', upload.single('attachment'), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ error: '파일이 업로드되지 않았습니다.' });
-  }
-  console.log(req.file);
-  res.json({ message: '파일 업로드 완료!', filePath: req.file.path });
-});
-
-app.use('/uploads', express.static(path.join(process.cwd(), 'src/uploads')));
 
 app.listen(process.env.PORT || 3000, () => console.log('Server Started'));
