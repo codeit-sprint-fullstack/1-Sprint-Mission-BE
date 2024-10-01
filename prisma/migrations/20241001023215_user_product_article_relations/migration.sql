70d72b4a6f0f25dbd820e5dfbf8e92046e24508e
minseokim <claudia503@naver.com>
2024-10-01 11:32:40 +0900

    feat: mission10 user_product_article_relations 마이그레이션

---
+++ prisma/migrations/20241001023215_user_product_article_relations/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "Article" ADD COLUMN     "userId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "userId" INTEGER;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Article" ADD CONSTRAINT "Article_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
