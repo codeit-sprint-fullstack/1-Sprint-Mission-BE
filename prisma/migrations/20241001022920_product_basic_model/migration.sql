1d58b378c3d8863ac318edef8ca53cff0b31b64a
minseokim <claudia503@naver.com>
2024-10-01 11:29:48 +0900

    feat: mission10 product_model 마이그레이션

---
+++ prisma/migrations/20241001022920_product_basic_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "image" TEXT,
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
