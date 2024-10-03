d0918fc0881e9a03235bf71c58a5d7b40b3c0f4e
kangbeomjoon <kobe7698@gmail.com>
2024-10-03 23:45:38 +0900

    미션10 백엔드 폴더구조와 파일구조

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// src/app.js
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
