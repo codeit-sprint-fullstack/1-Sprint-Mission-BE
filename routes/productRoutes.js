20685b0535caa772611d2687d2bfe0c0eea36667
minseokim <claudia503@naver.com>
2024-10-04 14:25:12 +0900

    fix: mission10_productRoutes.js 상품 라우트 수정

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const { createProduct, getProducts, getProductById, updateProduct, deleteProduct } = require('../controllers/productController');
-const upload = require('../utils/multer');
-const authMiddleware = require('../middlewares/authMiddleware');
+const express = require("express");
+const {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
+const authMiddleware = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-// 상품 등록 - 로그인한 사용자만 가능, 이미지 최대 3개
-router.post('/', authMiddleware, upload.array('images', 3), createProduct);
+// 상품 등록 - 로그인한 사용자만 가능
+router.post("/", authMiddleware, createProduct);
 
 // 모든 상품 목록 조회
-router.get('/', getProducts);
+router.get("/", getProducts);
 
 // 특정 상품 조회
-router.get('/:id', getProductById);
+router.get("/:productId", getProductById);
 
 // 상품 수정 - 로그인한 사용자만 가능
-router.patch('/:id', authMiddleware, upload.array('images', 3), updateProduct);
+router.patch("/:productId", authMiddleware, updateProduct);
 
 // 상품 삭제 - 로그인한 사용자만 가능
-router.delete('/:id', authMiddleware, deleteProduct);
+router.delete("/:productId", authMiddleware, deleteProduct);
 
 module.exports = router;
 
