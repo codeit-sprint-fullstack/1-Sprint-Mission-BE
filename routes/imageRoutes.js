44196b71330ca0975782692770440f243494774d
minseokim <claudia503@naver.com>
2024-10-04 15:48:39 +0900

    fix: mission10_imageRoutes.js upload를 multer에서 가져오도록 수정

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { upload, imageUpload } = require("../controllers/imageController");
+const upload = require("../utils/multer"); // upload는 multer에서 가져와야 함
+const { imageUpload } = require("../controllers/imageController");
 const router = express.Router();
 
 router.post("/upload", upload.single("image"), imageUpload);
