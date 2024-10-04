1040bb126c845473a5645513c733015d60336b52
minseokim <claudia503@naver.com>
2024-10-04 15:41:23 +0900

    feat: mission10_imageController.js 이미지 컨트롤러 추가

---
+++ controllers/imageController.js
@@ -0,0 +1,16 @@
+const upload = require('../utils/multer');
+
+// 이미지 업로드 처리
+const imageUpload = (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: '이미지를 업로드해 주세요.' });
+  }
+  const imageUrl = `/uploads/${req.file.filename}`;
+  res.status(200).json({ url: imageUrl });
+};
+
+module.exports = {
+  imageUpload,
+};
+
+
