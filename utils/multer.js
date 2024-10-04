b4e4f3f5357e696f08adb3756366013fcf8de0c2
minseokim <claudia503@naver.com>
2024-10-04 17:10:40 +0900

    fix: mission10_multer.js 이미지를 절대 경로로 변경

--- utils/multer.js
+++ utils/multer.js
@@ -4,7 +4,7 @@ const path = require('path');
 // 파일 저장 설정
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, path.join(__dirname, '../uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
