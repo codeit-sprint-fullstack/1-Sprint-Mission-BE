734351029f36a929a684e275d405071354648f4c
minseokim <claudia503@naver.com>
2024-10-06 20:28:54 +0900

    fix: mission10_imageCotroller.js 이미지 경로 서버 경로로

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,13 +1,13 @@
-// 이미지 업로드 처리
 const imageUpload = (req, res) => {
   if (!req.file) {
-    return res.status(400).json({ error: '이미지를 업로드해 주세요.' });
+    return res.status(400).json({ error: "이미지를 업로드해 주세요." });
   }
-  const imageUrl = `/uploads/${req.file.filename}`;
-  res.status(200).json({ url: imageUrl });
-};
 
-module.exports = {
-  imageUpload,
+  // 서버의 도메인 URL을 환경변수로 가져옴
+  const serverUrl = process.env.SERVER_URL || "https://baomarket.onrender.com";
+
+  // 절대 경로 생성
+  const imageUrl = `${serverUrl}/uploads/${req.file.filename}`;
+  res.status(200).json({ url: imageUrl });
 };
 
