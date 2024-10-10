358f7a2b3770cf6d2c6880e6bc0a50d2ae2a93e2
mini-1018 <anjaemin1018@gmail.com>
2024-10-10 20:38:11 +0900

    test7

--- structs.js
+++ structs.js
@@ -7,15 +7,6 @@ export const CreateArticle = s.object({
 
 export const PatchArticle = s.partial(CreateArticle);
 
-// export const CreateProduct = s.object({
-//   name: s.size(string(), 1, 10),
-//   description: s.size(string(), 1, 300),
-//   price: number(),
-//   tags: array(string()),
-// });
-
-export const PatchProduct = s.partial(CreateProduct);
-
 export const CreateComment = s.object({
   content: s.size(s.string(), 1, 50),
 });
