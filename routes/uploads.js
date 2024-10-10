f18c160fd23435cf8fcb4cb4fa3647d727ad9fef
mini-1018 <anjaemin1018@gmail.com>
2024-10-10 18:00:06 +0900

    feat : uploads 업로드 이미지 싱글에서 다수로 변경(배열)

--- routes/uploads.js
+++ routes/uploads.js
@@ -18,47 +18,47 @@ const s3 = new S3Client({
 
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/', upload.single('image'), async (req, res) => {
-  const file = req.file;
+router.post('/', upload.array('image', 3), async (req, res) => {
+  const files = req.file;
 
-  console.log('파일 정보:', req.file);
-
-  if (!file) {
+  if (!files) {
     return res.status(400).json({ message: '파일이 업로드되지 않았습니다.' });
   }
 
-  // S3 업로드 파라미터 설정
-  const uploadParams = {
-    Bucket: process.env.AWS_S3_BUCKET_NAME, // S3 버킷 이름
-    Key: `${Date.now()}_${file.originalname}`, // 파일명 중복 방지를 위해 타임스탬프 추가
-    Body: fs.createReadStream(file.path),
-    ContentType: file.mimetype, // 파일 타입 설정
-    ServerSideEncryption: 'AES256', // SSE-S3 암호화 설정
-  };
-
   try {
-    // S3에 파일 업로드
-    const command = new PutObjectCommand(uploadParams);
-    const data = await s3.send(command);
+    // 모든 파일을 S3에 업로드하고 URL을 배열로 저장
+    const uploadPromises = files.map(async (file) => {
+      // S3 업로드 파라미터 설정
+      const uploadParams = {
+        Bucket: process.env.AWS_S3_BUCKET_NAME, // S3 버킷 이름
+        Key: `${Date.now()}_${file.originalname}`, // 파일명 중복 방지를 위해 타임스탬프 추가
+        Body: fs.createReadStream(file.path),
+        ContentType: file.mimetype, // 파일 타입 설정
+        ServerSideEncryption: 'AES256', // SSE-S3 암호화 설정
+      };
 
-    // 로컬 파일 삭제
-    fs.unlink(file.path, (unlinkErr) => {
-      if (unlinkErr) {
-        console.error('로컬 파일 삭제 중 오류 발생:', unlinkErr);
-      }
+      // S3에 파일 업로드
+      const command = new PutObjectCommand(uploadParams);
+      await s3.send(command);
+
+      // 로컬 파일 삭제
+      fs.unlink(file.path, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error('로컬 파일 삭제 중 오류 발생:', unlinkErr);
+        }
+      });
+
+      // 업로드된 파일의 URL 반환
+      return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${uploadParams.Key}`;
     });
 
-    // 업로드된 파일의 URL 반환
-    const imageUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${uploadParams.Key}`;
-    res.status(200).json({ imageUrl });
+    // 모든 파일 업로드가 완료될 때까지 대기
+    const uploadedImageUrls = await Promise.all(uploadPromises);
+
+    // 업로드된 파일의 URL 배열을 클라이언트에 응답으로 반환
+    res.status(200).json({ imageUrls: uploadedImageUrls });
   } catch (err) {
     console.error('파일 업로드 오류:', err);
-    // 로컬 파일 삭제
-    fs.unlink(file.path, (unlinkErr) => {
-      if (unlinkErr) {
-        console.error('로컬 파일 삭제 중 오류 발생:', unlinkErr);
-      }
-    });
     return res
       .status(500)
       .json({ message: '파일 업로드 중 오류가 발생했습니다.' });
