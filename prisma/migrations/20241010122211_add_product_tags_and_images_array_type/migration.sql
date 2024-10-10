b5ca178d1821323295badc5a3b929a66647f3b4d
mini-1018 <anjaemin1018@gmail.com>
2024-10-10 21:22:38 +0900

    fix-상품 스키마

---
+++ prisma/migrations/20241010122211_add_product_tags_and_images_array_type/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The `tags` column on the `Product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `images` column on the `Product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "tags",
+ADD COLUMN     "tags" TEXT[],
+DROP COLUMN "images",
+ADD COLUMN     "images" TEXT[];
