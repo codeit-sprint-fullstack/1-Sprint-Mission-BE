54281d8522b7b5080940cba89ac69672be1a95ad
root <root@DESKTOP-UE7169K>
2024-11-06 17:33:14 +0900

    feat: 에러 핸들러 미들웨어 마이그레이션

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,49 @@
+import { Prisma } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import multer from "multer";
+
+interface CustomError extends Error {
+  status?: number;
+  message: string;
+  code?: string;
+  data?: string;
+}
+
+function errorHandler(
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  let status: number;
+
+  if (
+    error.name === "StructError" ||
+    error instanceof Prisma.PrismaClientValidationError ||
+    error instanceof multer.MulterError ||
+    error.code === "P2003"
+  ) {
+    status = 400;
+  } else if (error.name === "UnauthorizedError") {
+    status = 401;
+  } else if (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === "P2025"
+  ) {
+    status = 404;
+  } else if (error.code === "P2002") {
+    status = 409;
+  } else {
+    status = error.status ?? 500;
+  }
+
+  return res.status(status).json({
+    path: req.path,
+    method: req.method,
+    message: error.message ?? "Internal Server Error",
+    data: error.data ?? undefined,
+    date: new Date(),
+  });
+}
+
+export default errorHandler;
