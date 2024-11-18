7af20175cc95f578a51d697ffd2d65bda3e941cd
minseokim <claudia503@naver.com>
2024-11-18 15:58:35 +0900

    :recycle: [Refactor] Prisma 클라이언트 ESModules 지원을 위해 export default 추가

--- src/utils/prismaClient.ts
+++ src/utils/prismaClient.ts
@@ -1,6 +1,7 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 module.exports = prisma;
 
+export default prisma;
