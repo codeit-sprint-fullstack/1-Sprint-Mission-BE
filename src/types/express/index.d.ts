ecc1ed03dbbc9f9280e902a6ee667ce0f958bee1
minseokim <claudia503@naver.com>
2024-11-21 10:23:36 +0900

    :sparkles: [Feat] idex.d.ts MulterS3File 형식을 추가하여 S3 파일 업로드를 위한 Request 인터페이스 확장

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -1,10 +1,23 @@
-import { Multer } from "multer";
+import { Multer } from 'multer';
 
 declare global {
   namespace Express {
+    interface MulterS3File extends Multer.File {
+      bucket: string;
+      key: string;
+      acl: string;
+      contentType: string;
+      contentDisposition?: string;
+      storageClass: string;
+      serverSideEncryption?: string;
+      metadata: any;
+      location: string;
+      etag: string;
+    }
+
     interface Request {
       user?: { id: number }; // 사용자 정보를 담기 위한 속성
+      file?: MulterS3File;   // S3 파일 형식 확장
     }
   }
 }
-
