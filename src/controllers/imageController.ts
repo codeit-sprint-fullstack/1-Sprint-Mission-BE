98ee7f535b1f3c9a7d41ba80f48878edc7aa54f1
minseokim <claudia503@naver.com>
2024-11-21 11:11:43 +0900

    :sparkles: [Feat] imageController.ts Presigned URL 생성 시 파일 이름 검증 및 반환

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,19 +1,25 @@
 import { Request, Response } from 'express';
+import generatePresignedUrl from '../utils/multer';
 
 const ERROR_MESSAGES = {
   UPLOAD_REQUIRED: '이미지를 업로드해 주세요.',
 };
 
-const imageUpload = (req: Request, res: Response): void => {
-  const file = req.file as Express.MulterS3.File | undefined;
+const imageUpload = async (req: Request, res: Response): Promise<void> => {
+  const fileName = req.body.fileName; // 아이바오마켓에서 파일 이름을 받아올 예정
 
-  if (!file) {
+  if (!fileName) {
     res.status(400).json({ error: ERROR_MESSAGES.UPLOAD_REQUIRED });
     return;
   }
 
-  const imageUrl = file.location;
-  res.status(200).json({ url: imageUrl });
+  try {
+    const presignedUrl = await generatePresignedUrl(fileName);
+    res.status(200).json({ url: presignedUrl });
+  } catch (error) {
+    res.status(500).json({ error: 'URL 생성 중 오류 발생' });
+  }
 };
 
 export default imageUpload;
+
