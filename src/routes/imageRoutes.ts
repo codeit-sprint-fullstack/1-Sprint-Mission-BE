cdfd630b960f68deeef474e53514879bce03ed2e
minseokim <claudia503@naver.com>
2024-11-21 11:14:49 +0900

    :sparkles: [Feat] imageRoutes.ts Presigned URL 반환을 위한 라우터 설정

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import upload from '../utils/multer';
 import imageUpload from '../controllers/imageController';
 
 const router = express.Router();
 
-router.post('/upload', upload.single('image'), imageUpload);
+// 아이바오마켓이 S3에 직접 파일을 업로드할 수 있도록 Presigned URL을 반환하는 엔드포인트
+router.post('/upload', imageUpload);
 
 export default router;
