9281377f61531363386c7c6a15dac08d9b8a5faa
minseokim <claudia503@naver.com>
2024-11-21 10:16:05 +0900

    :recycle: [Refactor] imageRoutes.ts 이미지 업로드 라우터에서 middleware 방식으로 imageUpload 함수 호출 방식 변경

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -1,12 +1,9 @@
-import express, { Request, Response } from "express";
-import upload from "../utils/multer";
-import imageUpload from "../controllers/imageController";
+import express from 'express';
+import upload from '../utils/multer';
+import imageUpload from '../controllers/imageController';
 
 const router = express.Router();
 
-// 이미지 업로드
-router.post("/upload", upload.single("image"), (req: Request, res: Response) => {
-  imageUpload(req as Request, res);
-});
+router.post('/upload', upload.single('image'), imageUpload);
 
 export default router;
