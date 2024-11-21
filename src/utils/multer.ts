b65fee181a9d0bd07839baae406d2935eba86bc5
minseokim <claudia503@naver.com>
2024-11-21 10:27:28 +0900

    :sparkles: [Feat] multer.ts S3에 파일 업로드를 위한 multer 설정 추가 및 로컬 저장소에서 S3 저장소로 전환

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,36 +1,40 @@
-import multer from "multer";
-import path from "path";
-import fs from "fs";
+import multer from 'multer';
+import multerS3 from 'multer-s3';
+import { S3Client } from '@aws-sdk/client-s3';
+import path from 'path';
+import { Request } from 'express';
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, "../uploads");
+const UPLOAD_PATH = 'uploads/';
+const ALLOWED_EXTENSIONS = ['.jpg', '.jpeg', '.png'];
+const MAX_FILE_SIZE = 1024 * 1024 * 5; // 5MB
 
-    // 업로드 폴더가 존재하지 않으면 생성
-    if (!fs.existsSync(uploadPath)) {
-      fs.mkdirSync(uploadPath, { recursive: true });
-    }
-
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
+const s3 = new S3Client({
+  region: 'ap-northeast-2',
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   },
 });
 
-const fileFilter = (req: Express.Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-  const ext = path.extname(file.originalname).toLowerCase();
-  if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-    cb(null, false);
-  } else {
-    cb(null, true);
-  }
-};
-
 const upload = multer({
-  storage,
-  fileFilter,
-  limits: { fileSize: 1024 * 1024 * 5 }, // 최대 파일 크기 5MB 제한
+  storage: multerS3({
+    s3,
+    bucket: process.env.AWS_BUCKET_NAME as string,
+    acl: 'public-read',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req: Request, file, cb) => {
+      cb(null, `${UPLOAD_PATH}${Date.now()}-${file.originalname}`);
+    },
+  }),
+  fileFilter: (req: Request, file, cb) => {
+    const ext = path.extname(file.originalname).toLowerCase();
+    if (!ALLOWED_EXTENSIONS.includes(ext)) {
+      cb(null, false);
+    } else {
+      cb(null, true);
+    }
+  },
+  limits: { fileSize: MAX_FILE_SIZE },
 });
 
 export default upload;
